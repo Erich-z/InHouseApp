1d3dd5d52ffc6049ff213c40b0a1bd0815defba8
KAINAM NUNES DA SILVA SANTOS <kainam.nssantos@senacsp.edu.br>
2023-05-03 22:26:03 -0300

    HOME SCREEN

--- src/pages/TabHomeScreen.js
+++ src/pages/TabHomeScreen.js
@@ -1,37 +1,210 @@
-import React from 'react';
-import {Text, View, ScrollView,SafeAreaView} from 'react-native';
-import Carousel from 'react-native-snap-carousel';
 
-import styles from '../css/style';
+[
+  {
+
+    imgArray:['../img/Image1.jpg', '../img/Image2.jpg', '../img/Image3.jpg'],
+    city:'Presidente Prudente',
+    address:'AV Tititi',
+    price:220
+
+  },
+  {
+
+    imgArray:['../img/Image1.jpg', '../img/Image2.jpg', '../img/Image3.jpg'],
+    city:'Presidente Prudente',
+    address:'AV Tititi',
+    price:220
+
+  },
+]
 
 
+import React, {useState} from 'react';
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+} from 'react-native';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const images = [
-  require('../img/Image1.jpg'),
-  require('../img/Image1.jpg'),
-  require('../img/Image1.jpg'),
-  require('../img/Image1.jpg'),
+const images1 = [
+  {source: require('../img/Image1.jpg')},
+  {source: require('../img/Image2.jpg')},
+  {source: require('../img/Image3.jpg')},
+  {source: require('../img/Image4.jpg')},
 ];
 
-const TabHomeScreen = () => {
-    const renderItem = ({item, index}) => {
-        return (
-          <View style={styles.slide}>
-            <Image source={item} style={styles.image} />
+const images2 = [
+  {source: require('../img/Image2.jpg')},
+  {source: require('../img/Image3.jpg')},
+  {source: require('../img/Image4.jpg')},
+  {source: require('../img/Image1.jpg')},
+];
+
+const CarouselScreen = () => {
+  
+  const [activeSlide, setActiveSlide] = useState([0, 2]);
+                                                //  array[0] = 0, array[1] = 2
+
+  const renderItem = ({item}) => {
+    return (
+      <View style={styles.slide}>
+        <Image source={item.source} style={styles.image} />
+      </View>
+    );
+  };
+
+  return (
+    <ScrollView>
+      <View
+        style={{
+          marginBottom: 60,
+          top: 30,
+          width: 350,
+          flexDirection: 'row',
+          alignItems: 'center',
+          backgroundColor: '#d3d3d3',
+          borderRadius: 30,
+          paddingHorizontal: 10,
+        }}>
+        <TextInput style={{flex: 1, height: 40}} placeholder="Pesquisar..." />
+        <Icon name="search" size={20} color="gray" />
+      </View>
+
+      <View style={styles.container}>
+        <View style={styles.carousel}>
+
+          <Carousel
+            layout="default"
+            data={images1}
+            renderItem={renderItem}
+            sliderWidth={400}
+            itemWidth={400}
+            loop={true}
+            autoplay={false}
+            onSnapToItem={index => setActiveSlide(index)}
+          />
+          <View style={{position: 'absolute', bottom: 10, alignSelf: 'center'}}>
+            <Pagination
+              dotsLength={images1.length}
+              activeDotIndex={activeSlide}
+              containerStyle={{paddingVertical: 5}}
+              dotStyle={{
+                width: 15,
+                height: 15,
+                borderRadius: 10,
+                marginHorizontal: 8,
+                backgroundColor: 'rgba(255, 255, 255, 0.92)',
+              }}
+              inactiveDotStyle={{
+                backgroundColor: '#1e90ff',
+              }}
+              inactiveDotOpacity={0.4}
+              inactiveDotScale={0.6}
+            />
           </View>
-        );
-      };
-    
-      return (
-        <Carousel
-          layout="default"
-          data={images}
-          renderItem={renderItem}
-          sliderWidth={screenWidth}
-          itemWidth={screenWidth - 60}
-          loop={true}
-        />
-      );
+        
+        </View>
+
+        <View style={styles.textContainer}>
+          <Text style={[styles.text, {textAlign: 'left', fontWeight: 'bold'}]}>
+            Presidente Epitácio - SP
+          </Text>
+          <Text style={[styles.text, {textAlign: 'left'}]}>Av. Titititi</Text>
+          <Text style={[styles.text, {textAlign: 'left'}]}>R$200 / noite</Text>
+        </View>
+        <View style={styles.heartIconContainer}>
+          <Icon name="heart" size={25} color="red" />
+        </View>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.carousel}>
+
+          <Carousel
+            layout="default"
+            data={images2}
+            renderItem={renderItem}
+            sliderWidth={400}
+            itemWidth={400}
+            loop={true}
+            autoplay={false}
+            onSnapToItem={index => setActiveSlide(index)}
+          />
+          <View style={{position: 'absolute', bottom: 10, alignSelf: 'center'}}>
+            <Pagination
+              dotsLength={images2.length}
+              activeDotIndex={activeSlide}
+              containerStyle={{paddingVertical: 5}}
+              dotStyle={{
+                width: 15,
+                height: 15,
+                borderRadius: 10,
+                marginHorizontal: 8,
+                backgroundColor: 'rgba(255, 255, 255, 0.92)',
+              }}
+              inactiveDotStyle={{
+                backgroundColor: '#1e90ff',
+              }}
+              inactiveDotOpacity={0.4}
+              inactiveDotScale={0.6}
+            />
+          </View>
+        
+        </View>
+
+        <View style={styles.textContainer}>
+          <Text style={[styles.text, {textAlign: 'left', fontWeight: 'bold'}]}>
+            Presidente Epitácio - SP
+          </Text>
+          <Text style={[styles.text, {textAlign: 'left'}]}>Av. Titititi</Text>
+          <Text style={[styles.text, {textAlign: 'left'}]}>R$200 / noite</Text>
+        </View>
+        <View style={styles.heartIconContainer}>
+          <Icon name="heart" size={25} color="red" />
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
-export default TabHomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  slide: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  image: {
+    borderRadius: 20,
+    width: 350,
+    height: 200,
+    resizeMode: 'cover',
+  },
+  carousel: {
+    height: 200,
+    marginBottom: 10,
+  },
+  textContainer: {
+    flex: 1,
+    width: 350,
+    justifyContent: 'flex-end',
+  },
+  text: {
+    fontSize: 15,
+    color: '#000',
+  },
+  heartIconContainer: {
+    position: 'absolute',
+    top: 10,
+    right: 40,
+    zIndex: 1,
+  },
+});
+
+export default CarouselScreen;
