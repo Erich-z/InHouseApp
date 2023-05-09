2139853cbbdc54d018cca57ade24a48baa457735
GABRIEL AMORIM ROMAO <gabriel.aromao@senacsp.edu.br>
2023-05-08 22:13:55 -0300

    chat

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Text, View } from "react-native";
 import {NavigationContainer} from "@react-navigation/native"
 import MainTab from "./src/navigators/MainTab";
+import Chat from "./src/pages/Chat";
 
 function App(){
   return(
   <NavigationContainer>
-    <MainTab/>
+    <Chat/>
   </NavigationContainer>
   )
 }
