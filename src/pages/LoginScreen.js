274209164f911d96ca196bbcd408ea8e6d3c4deb
erich-z <erichrx0703@gmail.com>
2023-06-23 19:30:20 -0300

    Login e Cadastro com Redux

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,4 +1,4 @@
-import react from 'react';
+import react, {useEffect, useState} from 'react';
 import {Text, SafeAreaView, TextInput,View,} from 'react-native';
 import {
   Button,
@@ -15,10 +15,28 @@ import {
 import {styles} from '../css/style';
 import React from 'react';
 import {NativeBaseProvider} from 'native-base';
+import { useSelector, useDispatch } from "react-redux";
+import { loginRequest } from '../store/modules/auth/actions';
 
 import * as Icon from 'react-native-feather';
 
 function LoginIn({navigation}) {
+  const dispatch = useDispatch();
+  //manipular os dados do formulario dos inputs
+  const [dados, setDados] = useState({Email: '', senha: ''});
+  const handleChange = (text, nomeInput) => {
+    setDados({...dados,[nomeInput]: text});
+  }
+  //acao de fazer login ao clicar no botao entrar
+  const handleLogin = () => {
+    /*ConsomeApi.login(dados)
+    .then((token) => {
+      //console.log(token);
+      storeData('@login_token',token)
+    })
+    .catch(error => console.log(error));*/
+    dispatch(loginRequest(dados.Email,dados.senha));
+  }
   return (
     <NativeBaseProvider>
       <Center>
@@ -31,8 +49,10 @@ function LoginIn({navigation}) {
       </Center>
       <Center>
         <Stack space={2} w="90%" mx="auto">
-          <Input variant="underlined" placeholder="Enter your email" />
-          <Input variant="underlined" placeholder="Enter your password" />
+          <Input variant="underlined" placeholder="Enter your email" value={dados.Email}
+        onChangeText={(text)=> handleChange(text, 'Email')} />
+          <Input variant="underlined" placeholder="Enter your password" value={dados.senha}
+        onChangeText={(text)=> handleChange(text, 'senha')}/>
           <View style={{justifyContent: 'center',alignItems:'center', }}>
           <Text style={{fontWeight: 'bold', color: '#000'}}>Or</Text>
           </View>
@@ -45,7 +65,7 @@ function LoginIn({navigation}) {
           </Button>
           <Button size="sm" borderRadius='20' h="10" colorScheme="secondary" marginBottom='5'>Continue with Google</Button>
           <Center>
-          <Button size="sm" borderRadius='10' w="80%" h="10"  onPress={()=>navigation.push('Main')}>Sign In</Button>
+          <Button size="sm" borderRadius='10' w="80%" h="10"  onPress={()=> handleLogin()}>Sign In</Button>
           </Center>
           
           <View style={{alignItems: 'center', margin: 5}}>
