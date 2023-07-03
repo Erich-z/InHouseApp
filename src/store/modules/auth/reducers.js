c2950783c13cb6f54da31803beac60ee601b8cb7
erich-z <erichrx0703@gmail.com>
2023-07-03 19:40:57 -0300

    permissoes

--- src/store/modules/auth/reducers.js
+++ src/store/modules/auth/reducers.js
@@ -1,7 +1,7 @@
 import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from './actions';
 
 const initialState = {
-    isAuthenticated: false,
+    isAuthenticated: true,
     token: null,
     error: null
 }
